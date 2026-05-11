978ec2057737ece7e4430ede6db0641bfcee8ae3
CarlosNNVass <carlos.nunez@vasscompany.com>
2026-05-11 14:32:02 +0200

    seguro-avanza

--- blocks/hero-product/hero-product.js
+++ blocks/hero-product/hero-product.js
@@ -2,4 +2,11 @@ export default function decorate(block) {
   if (!block.querySelector(':scope > div:first-child picture')) {
     block.classList.add('no-image');
   }
+
+  const contentRow = block.querySelector(':scope > div:last-child');
+  const contentColumns = contentRow ? [...contentRow.children] : [];
+
+  if (contentColumns.length > 1) {
+    block.classList.add('has-aside');
+  }
 }
